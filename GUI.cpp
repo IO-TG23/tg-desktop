811627ac34fab7cce95c14833e9f52d79ffe99fb
mrogowski01 <michal.rogowski2611@gmail.com>
2023-04-27 16:21:18 +0200

    update branch
    install_wx.sh change

--- GUI.cpp
+++ GUI.cpp
@@ -21,6 +21,18 @@ main_frame::main_frame( wxWindow* parent, wxWindowID id, const wxString& title, 
 
 	bSizer5->Add( 10, 10, 0, wxEXPAND, 5 );
 
+	wxBoxSizer* bSizer25;
+	bSizer25 = new wxBoxSizer( wxVERTICAL );
+
+	logout = new wxButton( this, wxID_ANY, wxT("Wyloguj"), wxDefaultPosition, wxDefaultSize, 0 );
+	logout->SetForegroundColour( wxColour( 229, 229, 229 ) );
+	logout->SetBackgroundColour( wxColour( 51, 19, 11 ) );
+
+	bSizer25->Add( logout, 0, wxALL|wxALIGN_RIGHT, 5 );
+
+
+	bSizer5->Add( bSizer25, 0, wxEXPAND, 5 );
+
 	wxBoxSizer* bSizer13;
 	bSizer13 = new wxBoxSizer( wxHORIZONTAL );
 
@@ -86,6 +98,7 @@ main_frame::main_frame( wxWindow* parent, wxWindowID id, const wxString& title, 
 
 	// Connect Events
 	this->Connect( wxEVT_CLOSE_WINDOW, wxCloseEventHandler( main_frame::main_frameOnClose ) );
+	logout->Connect( wxEVT_COMMAND_BUTTON_CLICKED, wxCommandEventHandler( main_frame::logoutOnButtonClick ), NULL, this );
 	add_car_button->Connect( wxEVT_COMMAND_BUTTON_CLICKED, wxCommandEventHandler( main_frame::add_car_buttonOnButtonClick ), NULL, this );
 	import_button->Connect( wxEVT_COMMAND_BUTTON_CLICKED, wxCommandEventHandler( main_frame::import_buttonOnButtonClick ), NULL, this );
 	export_button->Connect( wxEVT_COMMAND_BUTTON_CLICKED, wxCommandEventHandler( main_frame::export_buttonOnButtonClick ), NULL, this );
@@ -95,6 +108,7 @@ main_frame::~main_frame()
 {
 	// Disconnect Events
 	this->Disconnect( wxEVT_CLOSE_WINDOW, wxCloseEventHandler( main_frame::main_frameOnClose ) );
+	logout->Disconnect( wxEVT_COMMAND_BUTTON_CLICKED, wxCommandEventHandler( main_frame::logoutOnButtonClick ), NULL, this );
 	add_car_button->Disconnect( wxEVT_COMMAND_BUTTON_CLICKED, wxCommandEventHandler( main_frame::add_car_buttonOnButtonClick ), NULL, this );
 	import_button->Disconnect( wxEVT_COMMAND_BUTTON_CLICKED, wxCommandEventHandler( main_frame::import_buttonOnButtonClick ), NULL, this );
 	export_button->Disconnect( wxEVT_COMMAND_BUTTON_CLICKED, wxCommandEventHandler( main_frame::export_buttonOnButtonClick ), NULL, this );
@@ -119,7 +133,7 @@ login_dial::login_dial( wxWindow* parent, wxWindowID id, const wxString& title, 
 	wxBoxSizer* bSizer3;
 	bSizer3 = new wxBoxSizer( wxHORIZONTAL );
 
-	m_staticText1 = new wxStaticText( this, wxID_ANY, wxT("Login"), wxDefaultPosition, wxDefaultSize, 0 );
+	m_staticText1 = new wxStaticText( this, wxID_ANY, wxT("e-mail"), wxDefaultPosition, wxDefaultSize, 0 );
 	m_staticText1->Wrap( -1 );
 	m_staticText1->SetForegroundColour( wxColour( 20, 6, 4 ) );
 
@@ -130,7 +144,7 @@ login_dial::login_dial( wxWindow* parent, wxWindowID id, const wxString& title, 
 	login_field->SetBackgroundColour( wxColour( 144, 144, 144 ) );
 	login_field->SetMinSize( wxSize( 150,-1 ) );
 
-	bSizer3->Add( login_field, 0, wxALIGN_CENTER_VERTICAL|wxALL, 5 );
+	bSizer3->Add( login_field, 0, wxALL|wxALIGN_BOTTOM, 5 );
 
 
 	bSizer2->Add( bSizer3, 1, wxALIGN_CENTER_HORIZONTAL, 5 );
@@ -149,7 +163,7 @@ login_dial::login_dial( wxWindow* parent, wxWindowID id, const wxString& title, 
 	passwd_field->SetBackgroundColour( wxColour( 144, 144, 144 ) );
 	passwd_field->SetMinSize( wxSize( 150,-1 ) );
 
-	bSizer31->Add( passwd_field, 0, wxALIGN_CENTER_VERTICAL|wxALL, 5 );
+	bSizer31->Add( passwd_field, 0, wxALL|wxALIGN_BOTTOM, 5 );
 
 
 	bSizer2->Add( bSizer31, 1, wxALIGN_CENTER_HORIZONTAL, 5 );
@@ -199,5 +213,123 @@ login_dial::~login_dial()
 	passwd_field->Disconnect( wxEVT_COMMAND_TEXT_MAXLEN, wxCommandEventHandler( login_dial::passwd_fieldOnTextMaxLen ), NULL, this );
 	login_button->Disconnect( wxEVT_COMMAND_BUTTON_CLICKED, wxCommandEventHandler( login_dial::login_buttonOnButtonClick ), NULL, this );
 	regiter_button->Disconnect( wxEVT_COMMAND_BUTTON_CLICKED, wxCommandEventHandler( login_dial::regiter_buttonOnButtonClick ), NULL, this );
+
+}
+
+register_dial::register_dial( wxWindow* parent, wxWindowID id, const wxString& title, const wxPoint& pos, const wxSize& size, long style ) : wxDialog( parent, id, title, pos, size, style )
+{
+	this->SetSizeHints( wxDefaultSize, wxDefaultSize );
+	this->SetForegroundColour( wxColour( 30, 11, 6 ) );
+	this->SetBackgroundColour( wxColour( 128, 48, 28 ) );
+
+	wxBoxSizer* bSizer1;
+	bSizer1 = new wxBoxSizer( wxVERTICAL );
+
+	wxBoxSizer* bSizer2;
+	bSizer2 = new wxBoxSizer( wxHORIZONTAL );
+
+
+	bSizer2->Add( 0, 20, 0, 0, 5 );
+
+	wxBoxSizer* bSizer21;
+	bSizer21 = new wxBoxSizer( wxVERTICAL );
+
+	m_staticText1 = new wxStaticText( this, wxID_ANY, wxT("e-mail"), wxDefaultPosition, wxDefaultSize, 0 );
+	m_staticText1->Wrap( -1 );
+	m_staticText1->SetForegroundColour( wxColour( 20, 6, 4 ) );
+
+	bSizer21->Add( m_staticText1, 1, wxALIGN_RIGHT|wxALL, 5 );
+
+	m_staticText11 = new wxStaticText( this, wxID_ANY, wxT("Hasło"), wxDefaultPosition, wxDefaultSize, 0 );
+	m_staticText11->Wrap( -1 );
+	m_staticText11->SetForegroundColour( wxColour( 20, 6, 4 ) );
+
+	bSizer21->Add( m_staticText11, 1, wxALIGN_RIGHT|wxALL, 5 );
+
+	m_staticText111 = new wxStaticText( this, wxID_ANY, wxT("Powtórz hasło"), wxDefaultPosition, wxDefaultSize, 0 );
+	m_staticText111->Wrap( -1 );
+	m_staticText111->SetForegroundColour( wxColour( 20, 6, 4 ) );
+
+	bSizer21->Add( m_staticText111, 1, wxALIGN_RIGHT|wxALL, 5 );
+
+
+	bSizer2->Add( bSizer21, 1, wxEXPAND, 5 );
+
+	wxBoxSizer* bSizer211;
+	bSizer211 = new wxBoxSizer( wxVERTICAL );
+
+	login_field = new wxTextCtrl( this, wxID_ANY, wxEmptyString, wxDefaultPosition, wxDefaultSize, wxTE_PROCESS_ENTER );
+	login_field->SetForegroundColour( wxColour( 77, 28, 16 ) );
+	login_field->SetBackgroundColour( wxColour( 144, 144, 144 ) );
+	login_field->SetMinSize( wxSize( 150,-1 ) );
+
+	bSizer211->Add( login_field, 0, wxALL|wxALIGN_CENTER_HORIZONTAL, 5 );
+
+	passwd_field1 = new wxTextCtrl( this, wxID_ANY, wxEmptyString, wxDefaultPosition, wxDefaultSize, wxTE_PASSWORD|wxTE_PROCESS_ENTER );
+	passwd_field1->SetForegroundColour( wxColour( 77, 28, 16 ) );
+	passwd_field1->SetBackgroundColour( wxColour( 144, 144, 144 ) );
+	passwd_field1->SetMinSize( wxSize( 150,-1 ) );
+
+	bSizer211->Add( passwd_field1, 0, wxALL|wxALIGN_CENTER_HORIZONTAL, 5 );
+
+	passwd2_field = new wxTextCtrl( this, wxID_ANY, wxEmptyString, wxDefaultPosition, wxDefaultSize, wxTE_PASSWORD|wxTE_PROCESS_ENTER );
+	passwd2_field->SetForegroundColour( wxColour( 77, 28, 16 ) );
+	passwd2_field->SetBackgroundColour( wxColour( 144, 144, 144 ) );
+	passwd2_field->SetMinSize( wxSize( 150,-1 ) );
+
+	bSizer211->Add( passwd2_field, 0, wxALL|wxALIGN_CENTER_HORIZONTAL, 5 );
+
+
+	bSizer2->Add( bSizer211, 1, wxEXPAND, 5 );
+
+
+	bSizer1->Add( bSizer2, 1, wxALL, 5 );
+
+
+	bSizer1->Add( 0, 15, 0, 0, 5 );
+
+	register_button = new wxButton( this, wxID_ANY, wxT("Zarejestruj"), wxDefaultPosition, wxDefaultSize, 0 );
+	register_button->SetForegroundColour( wxColour( 229, 229, 229 ) );
+	register_button->SetBackgroundColour( wxColour( 50, 19, 11 ) );
+
+	bSizer1->Add( register_button, 0, wxALL|wxALIGN_CENTER_HORIZONTAL, 5 );
+
+
+	this->SetSizer( bSizer1 );
+	this->Layout();
+	bSizer1->Fit( this );
+
+	this->Centre( wxBOTH );
+
+	// Connect Events
+	this->Connect( wxEVT_CLOSE_WINDOW, wxCloseEventHandler( register_dial::register_dialOnClose ) );
+	this->Connect( wxEVT_ICONIZE, wxIconizeEventHandler( register_dial::register_dialOnIconize ) );
+	login_field->Connect( wxEVT_COMMAND_TEXT_UPDATED, wxCommandEventHandler( register_dial::login_fieldOnText ), NULL, this );
+	login_field->Connect( wxEVT_COMMAND_TEXT_ENTER, wxCommandEventHandler( register_dial::login_fieldOnTextEnter ), NULL, this );
+	login_field->Connect( wxEVT_COMMAND_TEXT_MAXLEN, wxCommandEventHandler( register_dial::login_fieldOnTextMaxLen ), NULL, this );
+	passwd_field1->Connect( wxEVT_COMMAND_TEXT_UPDATED, wxCommandEventHandler( register_dial::passwd_field1OnText ), NULL, this );
+	passwd_field1->Connect( wxEVT_COMMAND_TEXT_ENTER, wxCommandEventHandler( register_dial::passwd_fieldOnTextEnter ), NULL, this );
+	passwd_field1->Connect( wxEVT_COMMAND_TEXT_MAXLEN, wxCommandEventHandler( register_dial::passwd_fieldOnTextMaxLen ), NULL, this );
+	passwd2_field->Connect( wxEVT_COMMAND_TEXT_UPDATED, wxCommandEventHandler( register_dial::passwd2_fieldOnText ), NULL, this );
+	passwd2_field->Connect( wxEVT_COMMAND_TEXT_ENTER, wxCommandEventHandler( register_dial::passwd2_fieldOnTextEnter ), NULL, this );
+	passwd2_field->Connect( wxEVT_COMMAND_TEXT_MAXLEN, wxCommandEventHandler( register_dial::passwd2_fieldOnTextMaxLen ), NULL, this );
+	register_button->Connect( wxEVT_COMMAND_BUTTON_CLICKED, wxCommandEventHandler( register_dial::register_buttonOnButtonClick ), NULL, this );
+}
+
+register_dial::~register_dial()
+{
+	// Disconnect Events
+	this->Disconnect( wxEVT_CLOSE_WINDOW, wxCloseEventHandler( register_dial::register_dialOnClose ) );
+	this->Disconnect( wxEVT_ICONIZE, wxIconizeEventHandler( register_dial::register_dialOnIconize ) );
+	login_field->Disconnect( wxEVT_COMMAND_TEXT_UPDATED, wxCommandEventHandler( register_dial::login_fieldOnText ), NULL, this );
+	login_field->Disconnect( wxEVT_COMMAND_TEXT_ENTER, wxCommandEventHandler( register_dial::login_fieldOnTextEnter ), NULL, this );
+	login_field->Disconnect( wxEVT_COMMAND_TEXT_MAXLEN, wxCommandEventHandler( register_dial::login_fieldOnTextMaxLen ), NULL, this );
+	passwd_field1->Disconnect( wxEVT_COMMAND_TEXT_UPDATED, wxCommandEventHandler( register_dial::passwd_field1OnText ), NULL, this );
+	passwd_field1->Disconnect( wxEVT_COMMAND_TEXT_ENTER, wxCommandEventHandler( register_dial::passwd_fieldOnTextEnter ), NULL, this );
+	passwd_field1->Disconnect( wxEVT_COMMAND_TEXT_MAXLEN, wxCommandEventHandler( register_dial::passwd_fieldOnTextMaxLen ), NULL, this );
+	passwd2_field->Disconnect( wxEVT_COMMAND_TEXT_UPDATED, wxCommandEventHandler( register_dial::passwd2_fieldOnText ), NULL, this );
+	passwd2_field->Disconnect( wxEVT_COMMAND_TEXT_ENTER, wxCommandEventHandler( register_dial::passwd2_fieldOnTextEnter ), NULL, this );
+	passwd2_field->Disconnect( wxEVT_COMMAND_TEXT_MAXLEN, wxCommandEventHandler( register_dial::passwd2_fieldOnTextMaxLen ), NULL, this );
+	register_button->Disconnect( wxEVT_COMMAND_BUTTON_CLICKED, wxCommandEventHandler( register_dial::register_buttonOnButtonClick ), NULL, this );
 
 }
--- TGdesk_login_dial.cpp
+++ TGdesk_login_dial.cpp
@@ -9,13 +9,16 @@ login_dial( parent )
 
 void TGdesk_login_dial::login_dialOnClose( wxCloseEvent& event )
 {
+if(!is_logged())
 GetParent()->Close();
+else
+GetParent()->Enable();
 Destroy();
 }
 
 void TGdesk_login_dial::login_fieldOnTextEnter( wxCommandEvent& event )
 {
-// TODO: Implement login_fieldOnTextEnter
+
 }
 
 void TGdesk_login_dial::login_fieldOnTextMaxLen( wxCommandEvent& event )
@@ -35,11 +38,27 @@ void TGdesk_login_dial::passwd_fieldOnTextMaxLen( wxCommandEvent& event )
 
 void TGdesk_login_dial::login_buttonOnButtonClick( wxCommandEvent& event )
 {
-// TODO: Implement login_buttonOnButtonClick
-Close();
+std::string login = login_field->GetValue().ToStdString();
+std::string psw = passwd_field->GetValue().ToStdString();
+
+if(!mail_check(login)){
+    loggedin=false;
+    wxMessageDialog dlg(this, "Niepoprawny adres e-mail.", wxMessageBoxCaptionStr, wxOK|wxICON_ERROR);
+    dlg.ShowModal();
+}else if(psw.size()==0){
+    wxMessageDialog dlg(this, wxT("Musisz podać hasło."), wxMessageBoxCaptionStr, wxOK|wxICON_ERROR);
+    dlg.ShowModal();
+} else {
+    loggedin=true;
+    Close();
+}
+
 }
 
 void TGdesk_login_dial::regiter_buttonOnButtonClick( wxCommandEvent& event )
 {
-// TODO: Implement regiter_buttonOnButtonClick
+wxDialog *signup = new TGdesk_register_dial(this);
+signup->Show(true);
+Disable();
 }
+
