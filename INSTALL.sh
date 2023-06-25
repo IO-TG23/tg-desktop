986eacb1cffca84732a0717aea39fc84e3ce4ef4
damian95a <damian95a@interia.pl>
2023-06-25 17:01:50 +0200

    Installation script.

--- INSTALL.sh
+++ INSTALL.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+
+if [ '$#' -ge '1' -a '$1' = 'download' ]; then
+    git clone https://github.com/IO-TG23/tg-desktop.git
+fi
+
 sudo apt-get update
 sudo apt-get install -y build-essential bzip2 libgtk2.0-dev libgtk-3-dev libsm6 x11-xserver-utils curl libcurl4-gnutls-dev libjsoncpp-dev libjsoncpp25 libjsoncpp-dev
 
